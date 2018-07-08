883ca3427f86315acf54583c8aa11f07bd3e0f8a
Mike van Leeuwen <mkvanleeuwen@gmail.com>
2018-07-08 16:08:28 +0200

    Making sure input characters are correctly processed on all platforms.

--- prompt.c
+++ prompt.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char *readline(char *prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char *cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char *unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char **argv) {
 
-    /* Print Version and Exit Information */
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
-    /* In a never ending loop */
     while (1) {
 
-        /* Output our prompt */
-        fputs("lispy> ", stdout);
+        /* Now in either case readline will be correctly defined */
+        char *input = readline("lispy> ");
+        add_history(input);
 
-        /* Read a line of user input of maximum size 2048 */
-        fgets(input, 2048, stdin);
+        printf("No you're a %s\n", input);
+        free(input);
 
-        /* Echo input back to user */
-        printf("No you're a %s", input);
     }
 
     return 0;
